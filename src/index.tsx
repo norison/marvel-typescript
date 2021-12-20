5d0e1da2d20de62faec35ff802c51e3baa371647
Ihor Matiev <ihor.matiev@gmail.com>
2021-12-20 10:46:25 +0200

    added styles and fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./styles/main.scss";
+
 ReactDOM.render(
   <React.StrictMode></React.StrictMode>,
   document.getElementById("root")
